cbe7a091d93a20860a0228ff4e81e669cf49ec66
Carlos Gil <carlosgil@Carloss-MacBook-Pro.local>
2022-05-23 22:44:12 +0900

    Create concrete classes

--- python_interfaces.py
+++ python_interfaces.py
@@ -6,3 +6,17 @@ class InformalParserInterface:
   def extract_text(self, full_file_path: str) -> dict:
     """Extracts text from the current file"""
     pass
+
+class PDFParser(InformalParserInterface):
+  def load_data_source(self, path: str, file_name: str) -> str:
+      return super().load_data_source(path, file_name)
+
+  def extract_text(self, full_file_path: str) -> dict:
+      return super().extract_text(full_file_path)
+
+class EmailParser(InformalParserInterface):
+  def load_data_source(self, path: str, file_name: str) -> str:
+      return super().load_data_source(path, file_name)
+
+  def extract_text_from_email(self, full_file_path: str) -> dict:
+      pass
