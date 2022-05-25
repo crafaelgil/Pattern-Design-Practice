fb5a4e51019ebdbf8d078d5dbd3e76d792c323de
Carlos Gil <carlosgil@Carloss-MacBook-Pro.local>
2022-05-25 22:49:36 +0900

    Begin implementation of 'Composite' design pattern

---
+++ composite_design_pattern.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Component(ABC):
+  @property
+  def parent(self) -> Component:
+    return self._parent
+
+  @parent.setter
+  def parent(self, parent: Component):
+    self._parent = parent
+
+  def add(self, component: Component) -> None:
+    pass
+
+  def remove(self, component: Component) -> None:
+    pass
+
+  def is_composite(self) -> bool:
+    return False
+
+  @abstractmethod
+  def operation(Self) -> str:
+    pass
